63f714207b4c5dd495e87e5a1ad4d2142ebee576
apickwick <106232621+apickwick@users.noreply.github.com>
2022-09-24 08:45:41 +0200

    Add files via upload

--- I3DShapesTool.Lib/Export/WavefrontObj.cs
+++ I3DShapesTool.Lib/Export/WavefrontObj.cs
@@ -20,8 +20,13 @@ namespace I3DShapesTool.Lib.Export
         private I3DVector[] Normals { get; set; }
 
         private I3DUV[] UVs { get; set; }
+        private I3DUV[] UVs2 { get; set; }
+        private I3DUV[] UVs3 { get; set; }
+        private I3DUV[] UVs4 { get; set; }
 
-        public WavefrontObj(I3DShape shape, string name, float scale = 100)
+        private I3DVector4[] Colors { get; set; }
+
+        public WavefrontObj(I3DShape shape, string name, float scale = 1)
         {
             Scale = scale;
 
@@ -39,6 +44,18 @@ namespace I3DShapesTool.Lib.Export
 
             if(shape.UVSets.Length > 0)
                 UVs = shape.UVSets[0];
+
+            if(shape.UVSets.Length > 1)
+                UVs2 = shape.UVSets[1];
+
+            if(shape.UVSets.Length > 2)
+                UVs3 = shape.UVSets[2];
+
+            if(shape.UVSets.Length > 3)
+                UVs4 = shape.UVSets[3];
+
+            if(shape.VertexColor != null)
+                Colors = shape.VertexColor;
         }
 
         /// <summary>
@@ -78,6 +95,38 @@ namespace I3DShapesTool.Lib.Export
                 UVs = newUV;
             }
 
+            if(UVs2 != null || newObj.UVs2 != null)
+            {
+                I3DUV[] newUV2 = new I3DUV[(UVs2?.Length ?? 0) + (newObj.UVs2?.Length ?? 0)];
+                UVs2?.CopyTo(newUV2, 0);
+                newObj.UVs2?.CopyTo(newUV2, UVs2?.Length ?? 0);
+                UVs2 = newUV2;
+            }
+
+            if(UVs3 != null || newObj.UVs3 != null)
+            {
+                I3DUV[] newUV3 = new I3DUV[(UVs3?.Length ?? 0) + (newObj.UVs3?.Length ?? 0)];
+                UVs3?.CopyTo(newUV3, 0);
+                newObj.UVs3?.CopyTo(newUV3, UVs3?.Length ?? 0);
+                UVs3 = newUV3;
+            }
+
+            if(UVs4 != null || newObj.UVs4 != null)
+            {
+                I3DUV[] newUV4 = new I3DUV[(UVs4?.Length ?? 0) + (newObj.UVs4?.Length ?? 0)];
+                UVs4?.CopyTo(newUV4, 0);
+                newObj.UVs4?.CopyTo(newUV4, UVs4?.Length ?? 0);
+                UVs4 = newUV4;
+            }
+
+            if(Colors != null || newObj.Colors != null)
+            {
+                I3DVector4[] newColor = new I3DVector4[(Colors?.Length ?? 0) + (newObj.Colors?.Length ?? 0)];
+                Colors?.CopyTo(newColor, 0);
+                newObj.Colors?.CopyTo(newColor, Colors?.Length ?? 0);
+                Colors = newColor;
+            }
+
             I3DTri[] newTris = new I3DTri[Triangles.Length + newObj.Triangles.Length];
             Triangles.CopyTo(newTris, 0);
             for(int i = 0; i < newObj.Triangles.Length; i++)
@@ -110,11 +159,32 @@ namespace I3DShapesTool.Lib.Export
 
         private void WriteVertex(StreamWriter s, I3DVector vec)
         {
-            s.WriteLine("v {0:F4} {1:F4} {2:F4}", vec.X * Scale, vec.Y * Scale, vec.Z * Scale);
+            s.WriteLine("v {0:F6} {1:F6} {2:F6}", vec.X * Scale, vec.Y * Scale, vec.Z * Scale);
         }
+
+        private void WriteVertexWithColor(StreamWriter s, I3DVector vec, I3DVector4 col)
+        {
+            s.WriteLine("v {0:F6} {1:F6} {2:F6} {3:F6} {4:F6} {5:F6}", vec.X * Scale, vec.Y * Scale, vec.Z * Scale, col.X, col.Y, col.Z);
+        }
+
         private static void WriteUV(StreamWriter s, I3DUV uv)
         {
             s.WriteLine("vt {0:F6} {1:F6}", uv.U, uv.V);
+        }
+
+        private static void WriteUV2(StreamWriter s, I3DUV uv)
+        {
+            s.WriteLine("vt2 {0:F6} {1:F6}", uv.U, uv.V);
+        }
+
+        private static void WriteUV3(StreamWriter s, I3DUV uv)
+        {
+            s.WriteLine("vt3 {0:F6} {1:F6}", uv.U, uv.V);
+        }
+
+        private static void WriteUV4(StreamWriter s, I3DUV uv)
+        {
+            s.WriteLine("vt4 {0:F6} {1:F6}", uv.U, uv.V);
         }
 
         private static void WriteNormal(StreamWriter s, I3DVector vec)
@@ -158,15 +228,48 @@ namespace I3DShapesTool.Lib.Export
                 s.WriteLine();
                 WriteGroup(s, "default");
                 s.WriteLine();
-                foreach(I3DVector t in Positions)
+
+                if(Colors == null)
                 {
-                    WriteVertex(s, t);
+                    foreach(I3DVector t in Positions)
+                    {
+                        WriteVertex(s, t);
+                    }
                 }
+                else
+                {
+                    for( int i = 0; i < Positions.Length; i++)
+                    {
+                        WriteVertexWithColor(s, Positions[i], Colors[i]);
+                    }
+                }
+
                 if(UVs != null)
                 {
                     foreach(I3DUV t in UVs)
                     {
                         WriteUV(s, t);
+                    }
+                }
+                if(UVs2 != null)
+                {
+                    foreach(I3DUV t in UVs2)
+                    {
+                        WriteUV2(s, t);
+                    }
+                }
+                if(UVs3 != null)
+                {
+                    foreach(I3DUV t in UVs3)
+                    {
+                        WriteUV3(s, t);
+                    }
+                }
+                if(UVs4 != null)
+                {
+                    foreach(I3DUV t in UVs4)
+                    {
+                        WriteUV4(s, t);
                     }
                 }
                 if(Normals != null)
