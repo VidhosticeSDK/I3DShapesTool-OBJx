fc87e870c098d46c7db5b7e0cec7cf40eab95b83
VidhosticeSDK <106232621+VidhosticeSDK@users.noreply.github.com>
2025-03-28 16:41:53 +0100

    Add files via upload

--- _build_.bat
+++ _build_.bat
@@ -5,6 +5,7 @@ taskkill /F /IM dotnet.exe
 REM Download:
 REM https://dotnet.microsoft.com/en-us/download/dotnet/6.0
 REM dotnet-sdk-6.0.401-win-x64.zip - OK
+REM dotnet-sdk-6.0.428-win-x64.zip - OK
 
 REM https://stackoverflow.com/questions/44074121/build-net-core-console-application-to-output-an-exe
 
@@ -14,6 +15,7 @@ REM   -p:PublishTrimmed={true|false} --self-contained {true|false}
 
 dotnet publish --output "I3DShapesTool-OBJx-EXE" --runtime win-x64 --configuration Release I3DShapesTool-OBJx-master
 
+echo.
 taskkill /F /IM dotnet.exe
 
 echo.
